946afce68fd50d7882f302fa0e51c1a9dd6c6177
root <root@freecariat.precariat.ludd.ltu.se>
2016-12-11 18:37:29 +0100

    Adds config references and moregit add showtime.py

--- src/showtime.py
+++ src/showtime.py
@@ -1,36 +1,39 @@
-import os
+import os, magic
 from werkzeug import secure_filename
 from flask import Flask, Response, request
 from config import Config
-from seated import send_post
 
 app =Flask(__name__)
 cfg = Config()
 
-@app.route("/ogg")
-def streamogg():
+@app.route("/ogg/<string:song>")
+def streamogg(song):
 	def generate():
-		with open("signals/elevatormusic.ogg", "rb") as fogg:
+		path = os.path.join(cfg.data_path,song)
+		with open(path, "rb") as fogg:
 			data = fogg.read(1024)
 			while data:
 				yield data
 				data = fogg.read(1024)
+
 	return Response(generate(), mimetype="audio/ogg")
 
 @app.route('/upload', methods=['POST'])
 def uploadfile():
 	""" Accept file from outside source. """
-
-	print "files:",request.files
 	for key in  request.files.iterkeys():
 		stream = request.files[key]
 
-		path = os.path.join("signals/",key)
+		path = os.path.join(cfg.data_path,key)
 		with open(path, "wb") as ofile:
-			while ofile:
-				ofile.write(stream.read(1024))
-		
+			data = stream.read(1024)
+			while len(data)>0:
+				ofile.write(data)
+				data = stream.read(1024)
 
+		# remove files that turn out to be non-ogg
+		if "audio/ogg" != magic.from_file(path, mime=True):
+			os.remove(path)
 	return "Done"
 
 if __name__ == "__main__":
